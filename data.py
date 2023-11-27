3676c05dc72ee91fac336f8ef7c78eb41dcd33c3
rohanjha04 <rohan.dharmendra.jha@gmail.com>
2023-11-27 17:28:55 +0530

    tried adding ineq constraints

--- data.py
+++ data.py
@@ -75,18 +75,24 @@ expected_returns = daily_returns.mean()
 #     total_returns += optimal_weights[i]*expected_returns[i]*(len(data['Adj Close']) - data['Adj Close'].isna().sum())[i]
 # print("Returns: ", total_returns)
 
-# regularised_markowitz = RegularisedMarkowitz(expected_returns, cov_matrix, 10, 1)
+# regularised_markowitz = RegularisedMarkowitz(expected_returns, cov_matrix, 5, 20)
 # regularised_optimal_weights = regularised_markowitz.optimal_weights(0.05)
 # print("Optimal Weights: ", regularised_optimal_weights)
-# print("Mean Return: ", regularised_markowitz.portfolio_return(optimal_weights))
-# # print("Portfolio Variance: ", regularised_markowitz.portfolio_variance(optimal_weights))
+# print("Mean Return: ", regularised_markowitz.portfolio_return(regularised_optimal_weights))
+# print("Portfolio Variance: ", regularised_markowitz.portfolio_variance(optimal_weights))
 # total_returns = 0
 # for i in range(len(regularised_optimal_weights)):
 #     total_returns += regularised_optimal_weights[i]*expected_returns[i]*(len(data['Adj Close']) - data['Adj Close'].isna().sum())[i]
 # print("Returns: ", total_returns)
 cov_matrix= np.array(cov_matrix)
 expected_returns= np.array(expected_returns)
-# expected_returns= expected_returns.reshape((1,expected_returns.shape[0]))
-# print(expected_returns.shape)
-x= solve_regularized_qp(cov_matrix, 10, 1, 0.5)
+expected_returns= expected_returns.reshape((1,expected_returns.shape[0]))
+# expected_returns= expected_returns.T
+# print(expected_returns.size)
+expected_returns= np.vstack([expected_returns, np.eye(expected_returns.shape[1])])
+target_return= np.zeros(expected_returns.shape[1])
+target_return= target_return.reshape((target_return.shape[0],1))
+target_return= np.vstack([0.05, target_return])
+print(target_return)
+x= solve_regularized_qp(cov_matrix, 10, 1, expected_returns, target_return)
 print("Optimal Weights: ", x)
--- regularised_markowitz_cvx.py
+++ regularised_markowitz_cvx.py
@@ -1,26 +1,31 @@
 import numpy as np
 from cvxopt import matrix, solvers
 
-def solve_regularized_qp(Q, l1, l2, target_return):
+def solve_regularized_qp(Q, l1, l2, expected_return, target_return):
     n = Q.shape[0]
     # Define the objective function
-    P = matrix(Q+l2 * np.eye(n))
+    P = matrix(1/2*(Q+2*l2 * np.eye(n)))
     q = matrix(np.ones(n) * l1)
     # Return constraint
-    G_return = matrix(-np.array([np.ones(n)]))
+    # G_return = matrix(-np.array([np.ones(n)]))
+    G_return= matrix(-expected_return)
     h_return = matrix(-target_return)
-    print(G_return)
+    # print(G_return)
 
     # Combine the constraints
     G = G_return
     h = h_return
-
+    # print(G)
+    # print(h)
     # Equality constraint sum(x) = 1
     A = matrix(np.ones((1, n)))
     b = matrix(1.0)
 
     # Solve the QP problem
     sol = solvers.qp(P, q, G, h, A, b)
+    solvers.options['kktreg'] = 1e-8
+    # solvers.options['show_progress'] = False
+    # solvers.options['abstol'] = 1e-7
 
     # Extract the solution (portfolio weights)
     x = np.array(sol['x']).flatten()
